cd0d48cc4ff73f5b496df3a100385162f3e58e1a
ramelot <103487183+cimdataram@users.noreply.github.com>
2022-05-26 20:20:07 +0200

    Add files via upload

---
+++ collatz-theory.py
@@ -0,0 +1,18 @@
+# Search a number that disproves Collatz theory :-D
+# Probably an endless loop due to the natural law of the theory
+num = 1
+test = 987654898765678876
+while num == 1:
+    num = test
+    while num != 1:
+        if num % 2 == 0:
+            num = int(num / 2)
+        else:
+            num = 3 * num +1
+        print(test, end=' : ')
+        print(num)
+    if num == 1:
+        test += 1
+else:
+    print('disprove number: ', test)
+
